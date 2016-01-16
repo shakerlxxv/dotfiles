e0163995c348fe27b56b4579d6f9f6d5a898a6cb
Brian Shaver <shakerlxxv@gmail.com>
2016-01-16 08:16:42 -0500

    added iex configuration file

--- install.sh
+++ install.sh
@@ -8,7 +8,12 @@ if [ -f ~/.bashrc ]; then
     mv ~/.bashrc ~/.bashrc.bak
 fi
 
+if [ -f ~/.iex.exs ]; then
+    mv ~/.iex.exs ~/.iex.exs.bak
+fi
+
 ( ln -s `pwd`/bash_profile ~/.bash_profile &&
     ln -s `pwd`/bashrc ~/.bashrc &&
+    ln -s `pwd`/iex.exs ~/.iex.exs &&
     echo "Install completed"
 ) || echo "Install failed"
